581fba91571afcf2d476bd19f560a474c1858165
jiiin <jiiin.cho.dev@gmail.com>
2022-04-01 11:27:10 +0200

    ProductDetail UI completed

--- src/component/ProductDetail.tsx
+++ src/component/ProductDetail.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../app/store";
+import { getProductList } from "../features/products/productsSlice";
+import { ProductItemT } from "../interface";
 
-const ProductDetail = () => <h1>Hello@</h1>;
+const ProductDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { productList } = useSelector((state: RootState) => state.productList);
+  const [product, setProduct] = useState<ProductItemT | undefined>(undefined);
+
+  useEffect(() => {
+    dispatch(getProductList());
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    const found = productList.find((product) => product.id.toString() === id);
+    setProduct(found);
+  }, [productList, id]);
+  const onAdd = () => {
+    console.log("onAdd click");
+  };
+  if (product) {
+    const { title, price, description, image } = product;
+    return (
+      <main className="detail cotainer-column">
+        <section className="container-center">
+          <img src={image} alt="product image" className="detail-img" />
+          <article className="detail-meta container-column-space-between">
+            <div>
+              <h3>{title}</h3>
+              <p>{price} kr</p>
+              <button className="btn-primary" onClick={onAdd}>
+                Add to cart
+              </button>
+            </div>
+            <div>
+              <h4>Product information</h4>
+              <p>{description}</p>
+            </div>
+          </article>
+        </section>
+      </main>
+    );
+  } else {
+    return <h1>Product not found!</h1>;
+  }
+};
 
 export default ProductDetail;
